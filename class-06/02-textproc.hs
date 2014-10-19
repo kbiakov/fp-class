a292d9507140788db54406cacdab486e057a7116
KBiakov <kbiakov@gmail.com>
2014-10-19 22:09:15 +0400

    Update 02-textproc.hs

--- class-06/02-textproc.hs
+++ class-06/02-textproc.hs
@@ -8,9 +8,60 @@
   соединяется с соответствующей строкой второго файла);
   5) генерация случайного текстового файла (случайность должна ограничиваться
   максимальным количеством строк в файле и символов в строке).
-  
+
   Все входные данные программы должны передаваться с помощью параметров
   командной строки.
 -}
 
-main = undefined
+import System.IO
+import System.Environment
+import System.Directory
+import System.Random
+import Data.List
+import Data.Char(toUpper)
+
+task :: Int -> [String] -> IO ()
+task 1 = countLines
+task 2 = appendStr
+task 3 = toUpper'
+task 4 = zipFile
+task 5 = rndFile
+
+main = do
+  (num:args) <- getArgs
+  task (read num) args
+
+-- 1
+countLines [fname] = do
+  contents <- readFile fname
+  print $ length . filter (== '\n') $ contents
+
+-- 2
+appendStr [fname, str] = do
+  appendFile fname $ '\n' : str
+
+-- 3
+toUpper' [fname] = do
+  contents <- readFile fname
+  putStr . unlines . map (map toUpper) $ lines contents
+
+-- 4
+zipStr :: String -> String -> String
+zipStr str1 str2 = unlines $ zipWith (++) (lines str1) (lines str2)
+
+zipFile [f1, f2] = do
+  put1 <- readFile f1
+  put2 <- readFile f2
+  putStr $ zipStr put1 put2
+
+-- 5
+rndFile [m, n, fname] = do
+  gen <- getStdGen
+  sequence_ $ take (rndN (read m) gen) $ repeat g
+  where
+    g :: IO ()
+    g = do
+      nGen <- newStdGen
+      appendFile fname $ ((take (rndN (read n) nGen) $ rndS nGen) ++ "\n")
+      rndS gen = randomRs ('a', 'z') gen :: [Char]
+      rndN n gen = fst $ randomR (1, n) gen :: Int
