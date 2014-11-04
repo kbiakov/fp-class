7e279e3e70ce61308accc2b1c7d71a7436a4775e
KBiakov <kbiakov@gmail.com>
2014-11-04 16:28:38 +0300

    Update main.hs

--- class-08/04-sets/main.hs
+++ class-08/04-sets/main.hs
@@ -3,15 +3,12 @@ import qualified ListSet as LS
 import qualified TreeSet as TS
 import System.Random
 
-operation :: (AbstractSet a) => a -> [Int] -> a
-operation set list = foldl (add) set list
-
 test1 :: (AbstractSet a) => a -> Bool
 test1 s = contains (add (add (add s 3) 7) 6) 7
 
 test2 :: (AbstractSet a) => a -> [Int] -> Bool
 test2 s list = contains set (head list) && contains set (head $ tail list)
-  where set = operation s list
+  where set = foldl (add) s list
 
 test3 :: (AbstractSet a) => a -> Bool
 test3 s = not $ contains (remove (add (add (add s 2) 2) 8) 2) 2
