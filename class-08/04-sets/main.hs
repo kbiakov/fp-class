612c5a8f1dadf093fb3f02323fa451a5a2c24829
KBiakov <kbiakov@gmail.com>
2014-11-04 16:33:23 +0300

    Update main.hs

--- class-08/04-sets/main.hs
+++ class-08/04-sets/main.hs
@@ -6,17 +6,16 @@ import System.Random
 test1 :: (AbstractSet a) => a -> Bool
 test1 s = contains (add (add (add s 3) 7) 6) 7
 
-test2 :: (AbstractSet a) => a -> [Int] -> Bool
-test2 s list = contains set (head list) && contains set (head $ tail list)
+test2 :: (AbstractSet a) => a -> Bool
+test2 s = not $ contains (remove (add (add (add s 2) 2) 8) 2) 2
+
+test3 :: (AbstractSet a) => a -> [Int] -> Bool
+test3 s list = contains set (head list) && contains set (head $ tail list)
   where set = foldl (add) s list
 
-test3 :: (AbstractSet a) => a -> Bool
-test3 s = not $ contains (remove (add (add (add s 2) 2) 8) 2) 2
-
 main = do
-  print $ test1 (empty :: LS.Set) && test1 (empty :: TS.Set)
   gen <- newStdGen
-  list <- take 10 $ randomRs (1, 100) gen
-  print $ test2 (empty :: LS.Set) list && test2 (empty :: TS.Set) list
-  print $ test3 (empty :: LS.Set) && test3 (empty :: TS.Set)
-  
+  let list = take 10 $ randomRs (1, 100) gen
+  print $ test1 (empty :: LS.Set) && test1 (empty :: TS.Set)
+  print $ test2 (empty :: LS.Set) && test2 (empty :: TS.Set)
+  print $ test3 (empty :: LS.Set) list && test3 (empty :: TS.Set) list
